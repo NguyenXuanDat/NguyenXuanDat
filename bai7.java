b8148ee6edfdae64531d9994e53030d2691319d9
NguyenXuanDat <78271926+NguyenXuanDat@users.noreply.github.com>
2021-01-31 09:25:46 +0700

    Add files via upload

---
+++ bai7.java
@@ -0,0 +1,12 @@
+package src;
+
+public class bai7 {
+    public static void main(String[] args){
+        int i=0;
+        System.out.println("20 số nguyên dương đầu tiên là: ");
+        for(i=0;i<21;i++){
+
+            System.out.println(i);
+        }
+    }
+}
